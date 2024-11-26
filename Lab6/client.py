c839509aae585e5e8ca59836af00e1fce12179c4
Barry <tun32913@temple.edu>
2024-11-26 14:21:52 -0500

    grabbing stuff from lab 5

---
+++ Lab6/client.py
@@ -0,0 +1,107 @@
+
+import socket
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from rsa import RSA
+
+
+class Client:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client = None
+
+        self.rsa = RSA(65537)  
+        self.public_key = (self.rsa.e, self.rsa.n)
+        self.private_key = self.rsa.d
+        self.server_public_key = None 
+       
+        
+
+    def connect(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((self.host, self.port))
+
+    def send(self, message: str):
+        # Sign the message with the client's private key
+        message_bytes = message.encode('utf-8')
+        signature = self.rsa.sign(message_bytes)
+        data = f"{message}|{signature}"
+        self.client.sendall(data.encode('utf-8'))
+        print("Sent signed message to server.")
+
+    def receive(self, buffer_size=1024):
+        # Receive a message and its signature
+        data = self.client.recv(2048).decode('utf-8')
+        message, signature = data.split("|")
+        message = message.encode('utf-8')
+        signature = int(signature)
+
+        # Verify the signature using the server's public key
+        is_valid = self.server_public_key.verify(signature, message)
+        if is_valid:
+            print("Signature verified successfully.")
+            return message.decode('utf-8')
+        else:
+            print("Signature verification failed.")
+            return None
+
+    def hash_message(self, message: str, key: bytes) -> str:  #take both message as well as the key from file 
+        digest = hashes.Hash(hashes.SHA256())
+        combined = message.encode('ascii') + key
+        digest.update(combined)
+        return digest.finalize().hex()
+
+    def exchange_keys(self):
+        # Receive the server's public key
+        server_key_data = self.client.recv(1024).decode('utf-8')
+        e, n = map(int, server_key_data.split('|'))
+        self.server_public_key = RSA(e, n)
+        print("Received server's public key.")
+
+        # Send the client's public key to the server
+        self.client.sendall(f"{self.public_key[0]}|{self.public_key[1]}".encode('utf-8'))
+        print("Sent client's public key to server.")
+
+    def close(self):
+        self.client.close()
+
+
+if __name__ == '__main__':
+    HOST, PORT = '10.109.21.177', 9999
+
+
+    client = Client(HOST, PORT)
+    client.connect()
+    print(f"Connected to server at {HOST}:{PORT}")
+    client.exchange_keys()
+   
+
+    intro = "Client has connected!"
+    client.send(intro)
+    print(f"Sent message: {intro}")
+
+    while True:
+            received_message = client.receive()
+            if received_message is None:
+                print("Message verification failed or message is empty.")
+                break
+            print(f"Client: {received_message}")
+
+
+            if received_message.lower() == 'end chat':
+                print("Chat ended by server.")
+                break
+        
+    
+            # read for message
+            message_to_send = input("You: ")
+            client.send(message_to_send)
+
+    
+            # terminate if sent message is "end chat"
+            if message_to_send.lower() == 'end chat':
+                print("Chat ended by client.")
+                break
+
+    client.close()
---
+++ Lab6/server (2).py
@@ -0,0 +1,113 @@
+
+import socket
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from rsa import RSA
+
+
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.server = None
+        self.client = None
+        self.key_bytes = None
+
+        self.rsa = RSA(65537)  # Using standard public exponent
+        self.public_key = (self.rsa.e, self.rsa.n)  # Public key
+        self.private_key = (self.rsa.d, self.rsa.n)  # Private key
+        self.aes_key = None
+        self.client_public_key = None
+
+    def start(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((self.host, self.port))
+        self.server.listen(5)
+
+    def accept(self):
+        self.client, addr = self.server.accept()
+        return addr
+    
+    def receive(self, buffer_size=1024):
+        # Receive a message and its signature
+        data = self.client.recv(2048).decode('utf-8')# bytes to string 
+        message, signature = data.split("|") # split recieved data into message and signature
+        message = message.encode('utf-8') # encode message into bytes (sig req original message) 
+        signature = int(signature) # convert signature to int
+
+        # Verify the signature using the client's public key
+        is_valid = self.client_public_key.verify(signature, message)
+        if is_valid:
+            print("Signature verified successfully.")
+            return message.decode('utf-8')
+        else:
+            print("Signature verification failed.")
+            return None
+    
+    def send(self, message: str):
+        message_bytes = message.encode('utf-8') 
+        signature = self.rsa.sign(message_bytes) # Sign the message with the server's private key
+        data = f"{message}|{signature}"
+        self.client.sendall(data.encode('utf-8')) # send signed message to client
+        print("Sent signed message to client.") 
+
+    def exchange_keys(self):
+         # Send the server's public key to the client
+        self.client.sendall(f"{self.public_key[0]}|{self.public_key[1]}".encode('utf-8'))
+        print("Sent server's public key to client.")
+
+        # Receive the client's public key
+        client_key_data = self.client.recv(1024).decode('utf-8')
+        e, n = map(int, client_key_data.split('|')) #converts both e and n to int
+        self.client_public_key = RSA(e, n) #initialize client's public key used to verify messages from client
+        print("Received client's public key.")
+
+    
+    def close(self):
+        self.client.close()
+        self.server.close()
+
+
+if __name__ == '__main__':
+    HOST, PORT = '10.109.21.177', 9999
+
+    server = Server(HOST, PORT)
+    server.start()
+    print(f"Server started at {HOST}:{PORT}")
+
+    addr = server.accept()
+    print(f"Accepted connection from {addr}")
+
+    server.exchange_keys()
+    
+    
+    while True:
+        
+            # Wait for a message
+            received_message = server.receive()
+            if received_message is None:
+                print("Message verification failed or message is empty.")
+                break
+            print(f"Client: {received_message}")
+
+
+            if received_message.lower() == 'end chat':
+                print("Chat ended by client.")
+                break
+        
+    
+            # read for message
+            message_to_send = input("You: ")
+            server.send(message_to_send)
+
+    
+            # terminate if sent message is "end chat"
+            if message_to_send.lower() == 'end chat':
+                print("Chat ended by server.")
+                break
+
+    
+
+    server.close()
+    
